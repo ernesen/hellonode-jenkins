8b22fd24be6678fcd8c5365bb7ff0ac6dd971ec5
Ernese Norelus <ernesen@yahoo.com>
2022-08-08 23:07:18 +0800

    Update groovy-file-name.groovy

--- groovy-file-name.groovy
+++ groovy-file-name.groovy
@@ -1,6 +1,26 @@
 node {
-    stage('Running Jenkinfile') {
+    stage('aws --version') {
         /* Let's make sure we have the repository cloned to our workspace */
-        sh 'node -v'
+        sh 'aws --version'
     }
+    
+    stage('Running kubectl') {
+        /* Let's make sure we have the repository cloned to our workspace */
+        sh 'kubectl'
+    }
+    
+    stage('kustomize version') {
+        /* Let's make sure we have the repository cloned to our workspace */
+        sh 'kustomize version'
+    }
+    
+    stage('mvn -version') {
+        /* Let's make sure we have the repository cloned to our workspace */
+        sh 'mvn -version'
+    } 
+    
+    stage('npm -version') {
+        /* Let's make sure we have the repository cloned to our workspace */
+        sh 'npm -v; node -v'
+    } 
 }  

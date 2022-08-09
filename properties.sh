32796f6c5a8ade9660fea810084ad1cfc067dc8d
Ernese Norelus <ernesen@yahoo.com>
2022-08-09 20:04:37 +0800

    Update properties.sh

--- properties.sh
+++ properties.sh
@@ -6,6 +6,6 @@ function prop {
     grep "${1}" ${file} | cut -d'=' -f2
 }
 
-echo "WORKSPACE = " $(prop 'BUILD')
+echo "BUILD     = " $(prop 'BUILD')
 echo "WORKSPACE = " $(prop 'WORKSPACE')
 echo "TAG       = " $(prop 'GIT_URL')
